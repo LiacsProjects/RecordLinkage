b7f5aae78a407482c749d02bb3de52a0f4b0f879
tijmenterbeek <t.terbeek@vva-informatisering.nl>
2022-11-10 12:27:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,4 @@ print("Filtering datasets...")
 df_geboorte = df_geboorte_unfiltered[["uuid", "Kind-Voornaam", "Kind-Tussenvoegsel", "Kind-Geslachtsnaam", "Kind-Plaats wonen", "Kind-Plaats geboorte", "Kind-Datum geboorte", "Kind-Geslacht", "Vader-Voornaam", "Vader-Tussenvoegsel", "Vader-Geslachtsnaam", "Vader-Plaats wonen", "Vader-Plaats geboorte", "Vader-Datum geboorte", "Vader-Geslacht", "Vader-Beroep", "Vader-Leeftijd", "Moeder-Voornaam", "Moeder-Tussenvoegsel", "Moeder-Geslachtsnaam", "Moeder-Plaats wonen", "Moeder-Plaats geboorte", "Moeder-Datum geboorte", "Moeder-Geslacht", "Moeder-Beroep", "Moeder-Leeftijd", "Getuige-Voornaam", "Getuige-Patroniem", "Getuige-Tussenvoegsel", "Getuige-Geslachtsnaam", "Getuige-Plaats geboorte", "Getuige-Plaats wonen", "Getuige-Datum geboorte", "Getuige-Geslacht", "Getuige-Beroep", "Getuige-Leeftijd"]]
 # df_huwelijk = df_huwelijk_unfiltered[[]]
 
-
-print(df_geboorte.loc[(df_geboorte['Kind-Voornaam'] == "Gerrit Jan") & (df_geboorte['Kind-Geslachtsnaam'] == "Beek")].iloc[0])
-
 # print(df_geboorte)
