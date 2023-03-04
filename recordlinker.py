2f6d3f6f4f6a17a2d5c324d405d32a79564d0b79
tijmenterbeek <90912921+ttbeek@users.noreply.github.com>
2023-03-04 15:40:46 +0100

    cleaned files

--- recordlinker.py
+++ recordlinker.py
@@ -5,6 +5,7 @@ import Levenshtein
 import csv
 import os
 import multiprocessing
+import re
 
 
 WHITELIST = set("abcdefghijklmnopqrstuvwxyz ")
@@ -43,37 +44,45 @@ class RecordLinker():
 
         self.df_pairs = pd.read_csv("data\\pairs.csv", sep=";")
 
-        print(f"""
+        print("""
+                     /)
+            /\___/\ ((
+            \`@_@'/  ))
+            {_:Y:.}_//""" +  re.sub(" +", " ", f"""
+            -----------(_)^-'(_)-----------------
+            WELCOME TO THE RECORDLINKER! :)
+            -------------------------------------
             -- maximum Levenshtein distance: {MAX_LEVENSTHEIN}
             -- mode 1 for hp-ho
             -- mode 2 for hp-b
             -- mode 3 for ho-b
             -- mode 4 for b-b
-        """)
+            -------------------------------------
+        """))
     
     
-    def get_period(self, mode, age=0):
+    def get_period(self, age=0):
         start = None
         end = None
-        if mode == 1:
+        if self.mode == 1:
             start = AGE_MOTHER_RANGE["min"]
             end = AGE_MOTHER_RANGE["max"] + AGE_MARRIED_RANGE["max"] - AGE_MOTHER_RANGE["min"]
             if age > 14:
                 start = AGE_MOTHER_RANGE["min"]
                 end = AGE_MOTHER_RANGE["max"] - age + AGE_MARRIED_RANGE["max"]
         
-        elif mode == 2:
+        elif self.mode == 2:
             start = max(0, AGE_MOTHER_RANGE["min"] - AGE_MOTHER_RANGE["max"])
             end = AGE_MOTHER_RANGE["max"] - AGE_MOTHER_RANGE["min"]
             if age != None:
                 start = max(0, AGE_MOTHER_RANGE["min"] - AGE_MOTHER_RANGE["max"])
                 end = AGE_MOTHER_RANGE["max"] - age
         
-        elif mode == 3:
+        elif self.mode == 3:
             start = AGE_MOTHER_RANGE["min"] - (AGE_MOTHER_RANGE["max"] + AGE_MARRIED_RANGE["max"])
             end = AGE_MOTHER_RANGE["max"] - (AGE_MOTHER_RANGE["min"] + AGE_MOTHER_RANGE["min"])
         
-        elif mode == 4:
+        elif self.mode == 4:
             start = AGE_MOTHER_RANGE["min"] - AGE_MOTHER_RANGE["max"]
             end = AGE_MOTHER_RANGE["max"] - AGE_MOTHER_RANGE["min"]
         
@@ -94,57 +103,102 @@ class RecordLinker():
         return df_references, df_potential_links
 
 
-    def find_links(self, mode):
-        df_references, df_potential_links = self.filter_pairs(mode)
+    def find_links_reference(self, reference):
+        links_certs = []
+        links_persons = []
+        # if reference.Index % 1000 == 0:
+        #     print(reference.year, reference.Index, len(self.links_certs))
 
-        for year in range(1811, 1951):
-            self.year_indexes[year] = df_potential_links.year.searchsorted(year)
-        
-        for reference in df_references.itertuples():
+        # Get period where a match can happen
+        period_relative = self.get_period(reference.woman_age)
+        period = {"start": max(1811, min(1950, reference.year + period_relative["start"])), "end": max(1811, min(1950, reference.year + period_relative["end"]))}
 
-            if reference.Index % 100 == 0:
-                print(reference.year, reference.Index, len(self.links_certs))
+        # Get index of year range
+        period_index = {"start": self.year_indexes[period["start"]], "end": self.year_indexes[period["end"]]}
 
-            # Get period where a match can happen
-            period_relative = self.get_period(mode, reference.woman_age)
-            period = {"start": max(1811, min(1950, reference.year + period_relative["start"])), "end": max(1811, min(1950, reference.year + period_relative["end"]))}
+        # Filter potential matches on year and first letters
+        df_potential_links_filtered = self.df_potential_links.iloc[period_index["start"]:period_index["end"]]
+        df_potential_links_filtered = df_potential_links_filtered[df_potential_links_filtered["first_letters"] == reference.first_letters]
 
-            # Get index of year range
-            period_index = {"start": self.year_indexes[period["start"]], "end": self.year_indexes[period["end"]]}
-
-            # Filter potential matches on year and first letters
-            df_potential_links_filtered = df_potential_links.iloc[period_index["start"]:period_index["end"]]
-            df_potential_links_filtered = df_potential_links_filtered[df_potential_links_filtered["first_letters"] == reference.first_letters]
-
-
-            for potential_link in df_potential_links_filtered.itertuples():
-                # Voor b-b moet er iets bedacht worden om aantal links te verkleinen
+        for potential_link in df_potential_links_filtered.itertuples():
+            # Voor b-b moet er iets bedacht worden om aantal links te verkleinen
+            if self.mode == 4:
                 if potential_link.uuid == reference.uuid:
                     continue
                 
-                if [mode, potential_link.man_uuid, reference.man_uuid, "m"] in self.links_persons:
+                if [self.mode, potential_link.man_uuid, reference.man_uuid, "m"] in self.links_persons:
                     continue
 
-                distance = Levenshtein.distance(reference.man + " " + reference.woman, potential_link.man + " " + potential_link.woman)
-                
-                if distance <= MAX_LEVENSTHEIN:
-                    self.links_certs.append([
-                        mode,
-                        reference.uuid,
-                        potential_link.uuid,
-                        reference.man_uuid,
-                        potential_link.man_uuid,
-                        reference.woman_uuid,
-                        potential_link.woman_uuid,
-                        distance, 
-                        len(reference.man + " " + reference.woman), 
-                        len(potential_link.man + " " + potential_link.woman),
-                        potential_link.year - reference.year])
+            distance = Levenshtein.distance(reference.man + " " + reference.woman, potential_link.man + " " + potential_link.woman)
+            
+            if distance <= MAX_LEVENSTHEIN:
+                links_certs.append([
+                    self.mode,
+                    reference.uuid,
+                    potential_link.uuid,
+                    reference.man_uuid,
+                    potential_link.man_uuid,
+                    reference.woman_uuid,
+                    potential_link.woman_uuid,
+                    distance, 
+                    len(reference.man + " " + reference.woman), 
+                    len(potential_link.man + " " + potential_link.woman),
+                    potential_link.year - reference.year])
 
-                    self.links_persons.append([mode, reference.man_uuid, potential_link.man_uuid, "m"])
-                    self.links_persons.append([mode, reference.woman_uuid, potential_link.woman_uuid, "v"])
+                links_persons.append([self.mode, reference.man_uuid, potential_link.man_uuid, "m"])
+                links_persons.append([self.mode, reference.woman_uuid, potential_link.woman_uuid, "v"])
+        return links_certs, links_persons
 
 
+    def find_links_batch(self, df_references_batch:pd.DataFrame):
+        links_certs_batch =  []
+        links_persons_batch = []
+
+        for reference in df_references_batch.itertuples():
+            links_certs, links_persons = self.find_links_reference(reference)
+            links_certs_batch += links_certs
+            links_persons_batch += links_persons
+
+        return df_references_batch, links_certs_batch, links_persons_batch
+
+
+    def find_links(self, mode, cpu_boost=False):
+        self.mode = mode
+        df_references, self.df_potential_links = self.filter_pairs(mode)
+
+        for year in range(1811, 1951):
+            self.year_indexes[year] = self.df_potential_links.year.searchsorted(year)
+        
+        if cpu_boost:
+            df_references_batched = np.array_split(df_references, multiprocessing.cpu_count() * 2)
+            print(len(df_references_batched))
+
+            with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+                for df_references_batch, links_certs_batch, links_persons_batch in pool.imap_unordered(self.find_links_batch, df_references_batched):
+                    self.links_certs += links_certs_batch
+                    self.links_persons += links_persons_batch
+
+                    print(re.sub(" +", " ", f"""
+                        -------------------------------------
+                        Batch has been processed
+                        Certificates in batch: {len(df_references_batch.index)}
+                        Links in batch: {len(links_certs_batch)}
+                        Total amount of links: {len(self.links_certs)}
+                    """))
+        else:
+            for reference in df_references.itertuples():
+                links_certs, links_persons = self.find_links_reference(reference)
+                self.links_certs += links_certs
+                self.links_persons += links_persons
+
+        print(re.sub(" +", " ", f"""
+            -------------------------------------
+            Run took {round(time() - self.start, 2)} seconds
+            {len(self.links_certs)} links found!
+        """))
+
+
+    def save_links(self):
         df_links_certs = pd.DataFrame(self.links_certs, columns=[
             "mode",
             "reference_uuid", 
@@ -164,19 +218,24 @@ class RecordLinker():
             "link_uuid", 
             "sex"])
         
-        df_links_certs.to_csv(unique_file_name(f"results\\Links Certs RecordLinker", "csv"), sep=";", index=False, quoting=csv.QUOTE_NONNUMERIC)
-        df_links_persons.to_csv(unique_file_name(f"results\\Links Persons RecordLinker", "csv"), sep=";", index=False, quoting=csv.QUOTE_NONNUMERIC)
-
-        print(f"""
+        path_result_certs = unique_file_name(f"results\\Links Certs RecordLinker", "csv")
+        df_links_certs.to_csv(path_result_certs, sep=";", index=False, quoting=csv.QUOTE_NONNUMERIC)
+        print(re.sub(" +", " ", f"""
             -------------------------------------
-            Run took {round(time() - self.start, 2)} seconds
-            {len(df_links_certs.index)} links found!
-        """)
+            Saved person link at {path_result_certs}
+        """))
+
+        path_result_persons = unique_file_name(f"results\\Links Persons RecordLinker", "csv")
+        df_links_persons.to_csv(path_result_persons, sep=";", index=False, quoting=csv.QUOTE_NONNUMERIC)
+        print(re.sub(" +", " ", f"""
+            -------------------------------------
+            Saved person link at {path_result_persons}
+        """))
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     multiprocessing.freeze_support()
     linker = RecordLinker()
-    linker.find_links(4)
+    linker.find_links(4, True)
+    linker.save_links()
 
