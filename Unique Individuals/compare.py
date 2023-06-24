2e687e4efb88ec76e59809121ee5fcd9880a1380
tijmenterbeek <90912921+ttbeek@users.noreply.github.com>
2023-06-24 15:54:54 +0200

    idk

--- Unique Individuals/compare.py
+++ Unique Individuals/compare.py
@@ -129,101 +129,104 @@ def analyse_timelines(linking_method):
     last_year = max(col_years)
 
     person = []
-    current_id = 1
+    current_id = 0
 
-    duration = []
+    data = {
+        "born": [],
+        "married": [],
+        "child": [],
+        "died": [],
+        "duration": [],
+        "born_first": 0,
+        "not_born_first": 0,
+        "complete": 0,
+        "only_birth": 0,
+        "only_death": 0,
+        "no_birth_death": 0,
+        "incorrect": 0,
+        "complete_per_year": [],
+        "only_birth_per_year": [],
+        "only_death_per_year": [],
+        "no_birth_death_per_year": [],
+        "incorrect_per_year": [],
+        "alive_per_year": []
+    }
 
-    alive_per_year = []
 
-    incorrect_per_year = []
-    complete_per_year = []
-    only_birth_per_year = []
-    only_death_per_year = []
-    no_birth_death_per_year = []
+    def process_life_course(person, data):
+        documented_years = [year for role, year in person if role not in [4, 6, 7]]
+        if len(documented_years) == 0:
+            documented_years = [0]
 
-    born = []
-    married = []
-    child = []
-    died = []
+        max_year = max(documented_years)
+        min_year = min(documented_years)
+        range_life_course = list(range(min_year, max_year + 1))
 
-    birth_not_first = 0
-    birth_first = 0
+        data["alive_per_year"] += range_life_course
 
-    incorrect = 0
-    complete = 0
-    only_birth = 0
-    only_death = 0
-    no_birth_death = 0
+        times_born = len([role for role, _ in person if role == 1])
+        times_married = len([role for role, _ in person if role == 3])
+        times_child = len([role for role, _ in person if role == 2])
+        times_died = len([role for role, _ in person if role == 5])
+
+        data["born"].append(times_born)
+        data["married"].append(times_married)
+        data["child"].append(times_child)
+        data["died"].append(times_died)
+        data["duration"].append(max_year - min_year)
+
+        if times_born == 1 and times_died == 1:
+            data["complete"] += 1
+            data["complete_per_year"] += range_life_course
+        elif times_born == 1 and times_died == 0:
+            data["only_birth"] += 1
+            data["only_birth_per_year"] += range_life_course
+        elif times_born == 0 and times_died == 1:
+            data["only_death"] += 1
+            data["only_death_per_year"] += range_life_course
+        elif times_born == 0 and times_died == 0:
+            data["no_birth_death"] += 1
+            data["no_birth_death_per_year"] += range_life_course
+        elif times_born > 1 or times_died > 1:
+            data["incorrect"] += 1
+            data["incorrect_per_year"] += range_life_course
+
+        if times_born == 1:
+            birth_year = [year for role, year in person if role == 1][0]
+            if birth_year == min_year:
+                data["born_first"] += 1
+            else:
+                data["not_born_first"] += 1
+
 
     for reference in df_references.itertuples():
         if reference.unique_person_id != current_id:
-            documented_years = [year for role, year in person if role not in [4, 6, 7]]
-            if len(documented_years) == 0:
-                documented_years = [0]
-
-            max_year = max(documented_years)
-            min_year = min(documented_years)
-            range_life_course = list(range(min_year, max_year + 1))
-
-            alive_per_year += range_life_course
-
-            times_born = len([role for role, _ in person if role == 1])
-            times_married = len([role for role, _ in person if role == 3])
-            times_child = len([role for role, _ in person if role == 2])
-            times_died = len([role for role, _ in person if role == 5])
-
-            born.append(times_born)
-            married.append(times_married)
-            child.append(times_child)
-            died.append(times_died)
-
-            if times_born == 1 and times_died == 1:
-                complete += 1
-                complete_per_year += range_life_course
-            elif times_born == 1 and times_died == 0:
-                only_birth += 1
-                only_birth_per_year += range_life_course
-            elif times_born == 0 and times_died == 1:
-                only_death += 1
-                only_death_per_year += range_life_course
-            elif times_born == 0 and times_died == 0:
-                no_birth_death += 1
-                no_birth_death_per_year += range_life_course
-            elif times_born > 1 or times_died > 1:
-                incorrect += 1
-                incorrect_per_year += range_life_course
-
-            if times_born == 1:
-                birth_year = [year for role, year in person if role == 1][0]
-                if birth_year == min_year:
-                    birth_first += 1
-                else:
-                    birth_not_first += 1
-
-            duration.append(max_year - min_year)
-
-            current_id += 1
+            process_life_course(person, data)
+            current_id = reference.unique_person_id
             person = []
-            
+
         person.append((reference.role, reference.year))
 
-
+    process_life_course(person, data)
+    
     life_course_histograms = {
-        "born": born,
-        "married": married,
-        "child": child,
-        "died": died,
-        "duration": duration
+        "born": data["born"],
+        "married": data["married"],
+        "child": data["child"],
+        "died": data["died"],
+        "duration": data["duration"]
     }
 
     year_histograms = {
-        "complete": complete_per_year,
-        "only birth": only_birth_per_year,
-        "only death": only_death_per_year,
-        "no birth/death": no_birth_death_per_year,
-        "incorrect": incorrect_per_year,
-        "alive": alive_per_year
+        "complete": data["complete_per_year"],
+        "only birth": data["only_birth_per_year"],
+        "only death": data["only_death_per_year"],
+        "no birth/death": data["no_birth_death_per_year"],
+        "incorrect": data["incorrect_per_year"],
+        "alive": data["alive_per_year"]
     }
+
+
 
     frequencies = pd.DataFrame(0, columns=year_histograms.keys(), index=[0] + list(range(first_year, last_year + 1)))
 
@@ -233,7 +236,7 @@ def analyse_timelines(linking_method):
         for key, value in frequency.items():
             frequencies.at[key, hist_type] = value
 
-    frequencies.to_csv(unique_file_name(f"Unique Individuals\\{linking_method} Histogram life courses", "csv"), sep=";", quoting=csv.QUOTE_NONNUMERIC)
+    frequencies.to_csv(unique_file_name(f"Unique Individuals\\Histograms\\{linking_method} Histogram life courses", "csv"), sep=";", quoting=csv.QUOTE_NONNUMERIC)
 
     for hist_type, hist_data in life_course_histograms.items(): 
         frequency = collections.Counter(hist_data)
@@ -246,24 +249,24 @@ def analyse_timelines(linking_method):
         for key, value in frequency.items():
             frequencies.at[key, hist_type] = value
         
-        frequencies.to_csv(unique_file_name(f"Unique Individuals\\{linking_method} Histogram {hist_type}", "csv"), sep=";", quoting=csv.QUOTE_NONNUMERIC)
+        frequencies.to_csv(unique_file_name(f"Unique Individuals\\Histograms\\{linking_method} Histogram {hist_type}", "csv"), sep=";", quoting=csv.QUOTE_NONNUMERIC)
 
     print(linking_method)
-    print("Complete:", complete)
-    print("Only birth:", only_birth)
-    print("Only death:", only_death)
-    print("No birth/death:", no_birth_death)
-    print("Incorrect:", incorrect)
-    print("Som:", complete + only_birth + only_death + no_birth_death + incorrect)
+    print("Complete:", data["complete"])
+    print("Only birth:", data["only_birth"])
+    print("Only death:", data["only_death"])
+    print("No birth/death:", data["no_birth_death"])
+    print("Incorrect:", data["incorrect"])
+    print("Som:", data["complete"] + data["only_birth"] + data["only_death"] + data["no_birth_death"] + data["incorrect"])
 
-    print(birth_first, birth_not_first)
+    print(data["born_first"], data["not_born_first"])
     # print(total1, total2)
     # print(sum(alive_per_year), sum(complete_per_year) + sum(only_birth_per_year) + sum(only_death_per_year) + sum(incorrect_per_year) + sum(no_birth_death_per_year))
     print("-------------------\n")
 
 
-def get_role_hist(linker):
-    results = pd.read_csv(f"Unique Individuals\\results\\{linker} Detailed.csv", sep=";")
+def get_role_hist(linking_method):
+    results = pd.read_csv(f"Unique Individuals\\results\\{linking_method} Detailed.csv", sep=";")
     hist = {}
 
     for size in results["role"]:
@@ -272,18 +275,34 @@ def get_role_hist(linker):
         except:
             hist[size] = 1
 
-    print(linker)
+    print(linking_method)
+    for i in dict(sorted(hist.items())):
+        print(f"{i}, {hist[i]}")
+
+
+def get_role_hist_pairs():
+    references = get_detailed_references()
+    hist = {}
+
+    for value in references.values():
+        try:
+            hist[value["role"]] = hist[value["role"]] + 1
+        except:
+            hist[value["role"]] = 1
+    
+    print("Pairs:")
     for i in dict(sorted(hist.items())):
         print(f"{i}, {hist[i]}")
 
 
 # get_role_hist("BL")
 # get_role_hist("RL")
+# get_role_hist_pairs()
 
 # get_timelines("RL")
 # get_timelines("BL")
 
-analyse_timelines("RL")
+# analyse_timelines("RL")
 analyse_timelines("BL")
 
 # compare_groups()
